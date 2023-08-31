51c801d9002a3508ca4f2310c6b4d73ca529e462
KevinKalb <kevin.kalb@aponia.de>
2023-08-31 13:44:47 +0200

    more styling and input form for changing elements dynamically

--- lib/kkalb_web/live/index.ex
+++ lib/kkalb_web/live/index.ex
@@ -5,84 +5,109 @@ defmodule KkalbWeb.Live.Index do
 
   @impl true
   def mount(_params, _session, socket) do
+    elements_to_query = 10
+
     if connected?(socket) do
-      amount_elements = 10
-
-      result =
-        HTTPoison.get!(
-          "https://api.github.com/search/issues?per_page=#{amount_elements}&q=repo:elixir-lang/elixir+type:issue+state:open"
-        )
-        |> Map.get(:body)
-        |> Jason.decode!()
-
-      amount_issues = result |> Map.get("total_count")
-
-      items =
-        result
-        |> Map.get("items")
-        |> Enum.map(
-          &%{
-            "created_at" => &1["created_at"] |> parse_date_time(),
-            "closed_at" => &1["closed_at"] |> parse_date_time()
-          }
-        )
-
-      earliest_date =
-        Enum.min_by(items, fn dates_map -> dates_map["created_at"] end, Date)["created_at"]
-
-      latest_date =
-        Enum.max_by(items, fn dates_map -> dates_map["created_at"] end, Date)["created_at"]
-
-      range = Date.range(latest_date, earliest_date)
-      labels = range |> Enum.to_list()
-
-      values_diff =
-        for date_iter <- labels do
-          closed_this_date = Enum.count(items, fn item -> item["closed_at"] == date_iter end)
-          created_that_date = Enum.count(items, fn item -> item["created_at"] == date_iter end)
-
-          created_that_date + closed_this_date * -1
-        end
-
-      # example value for values_diff: [0, 0, -1, 0, 1, 1, 0, -2, 2, 1, 1] (desc)
-      {_, values} =
-        Enum.reduce(values_diff, {0, [amount_issues]}, fn x, acc ->
-          {new_diff, new_ele} =
-            case acc do
-              {0, [amount_issues]} ->
-                {x, [amount_issues, amount_issues + x]}
-
-              {diff, l} ->
-                last_ele = List.last(l)
-                {diff + x, List.insert_at(l, -1, last_ele + x)}
-            end
-
-          {new_diff, new_ele}
-        end)
-
-      chart_headings = %{headings: ["# of Elixir issues on Github"]}
-      chart_labels = %{labels: Enum.reverse(labels)}
-      chart_values = %{values: Enum.reverse(values)}
-      chart_title = ""
+      socket = build_chart(socket, elements_to_query)
 
       {:ok,
        socket
-       |> assign(
-         chart_headings: chart_headings,
-         chart_labels: chart_labels,
-         chart_values: chart_values,
-         chart_title: chart_title
-       )}
+       |> assign(elements_to_query: elements_to_query)}
     else
       {:ok,
-       socket
-       |> assign(
+       %{
          chart_headings: %{headings: ["Pulling data..."]},
          chart_labels: %{labels: [""]},
          chart_values: %{values: []},
          chart_title: "Pulling data..."
-       )}
+       }
+       |> assign_chart_data(socket)
+       |> assign(elements_to_query: elements_to_query)}
     end
+  end
+
+  defp request_github(elements_to_query) do
+    HTTPoison.get!(
+      "https://api.github.com/search/issues?per_page=#{elements_to_query}&q=repo:elixir-lang/elixir+type:issue+state:open"
+    )
+  end
+
+  @impl true
+  def handle_event("change_elements_to_query", %{"change_elements_to_query" => elements}, socket) do
+    {:noreply, socket |> assign(elements_to_query: elements) |> build_chart(elements)}
+  end
+
+  defp build_chart(socket, elements_to_query) do
+    elements_to_query
+    |> request_github()
+    |> Map.get(:body)
+    |> Jason.decode!()
+    |> transform()
+    |> assign_chart_data(socket)
+  end
+
+  defp transform(query_result) do
+    amount_issues = query_result |> Map.get("total_count")
+
+    items =
+      query_result
+      |> Map.get("items")
+      |> Enum.map(
+        &%{
+          "created_at" => &1["created_at"] |> parse_date_time(),
+          "closed_at" => &1["closed_at"] |> parse_date_time()
+        }
+      )
+
+    earliest_date =
+      Enum.min_by(items, fn dates_map -> dates_map["created_at"] end, Date)["created_at"]
+
+    latest_date =
+      Enum.max_by(items, fn dates_map -> dates_map["created_at"] end, Date)["created_at"]
+
+    range = Date.range(latest_date, earliest_date)
+    labels = range |> Enum.to_list()
+
+    values_diff =
+      for date_iter <- labels do
+        closed_this_date = Enum.count(items, fn item -> item["closed_at"] == date_iter end)
+        created_that_date = Enum.count(items, fn item -> item["created_at"] == date_iter end)
+
+        created_that_date + closed_this_date * -1
+      end
+
+    # example value for values_diff: [0, 0, -1, 0, 1, 1, 0, -2, 2, 1, 1] (desc)
+    {_, values} =
+      Enum.reduce(values_diff, {0, [amount_issues]}, fn x, acc ->
+        {new_diff, new_ele} =
+          case acc do
+            {0, [amount_issues]} ->
+              {x, [amount_issues, amount_issues + x]}
+
+            {diff, l} ->
+              last_ele = List.last(l)
+              {diff + x, List.insert_at(l, -1, last_ele + x)}
+          end
+
+        {new_diff, new_ele}
+      end)
+
+    %{
+      chart_headings: %{headings: ["# of Elixir issues on Github"]},
+      chart_labels: %{labels: Enum.reverse(labels)},
+      chart_values: %{values: Enum.reverse(values)},
+      chart_title: ""
+    }
+  end
+
+  defp assign_chart_data(chart_data, socket) do
+    socket
+    |> assign(
+      chart_headings: chart_data.chart_headings,
+      chart_labels: chart_data.chart_labels,
+      chart_values: chart_data.chart_values,
+      chart_title: chart_data.chart_title
+    )
   end
 
   defp parse_date_time(nil), do: nil
