eef80349b0ef44877864b781392f8e3cf60127d6
kkalb <kevinkalb17@googlemail.com>
2024-10-17 06:18:20 +0000

    change dir of portfolio

--- lib/kkalb_web/controllers/download.ex
+++ lib/kkalb_web/controllers/download.ex
@@ -3,7 +3,7 @@ defmodule KkalbWeb.DownloadController do
   use KkalbWeb, :controller
 
   def download_portfolio(conn, _) do
-    file = "priv/static/Kevin_Kalb_CV_2024_10.pdf"
+    file = "priv/static/images/Kevin_Kalb_CV_2024_10.pdf"
     send_download(conn, {:file, file})
   end
 end
