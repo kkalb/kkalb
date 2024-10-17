7f6585f6d4eb9f6fcffcc9ea8f793bb597bbbe95
kkalb <kevinkalb17@googlemail.com>
2024-10-17 12:01:00 +0000

    fixing portfolio download

--- lib/kkalb_web/controllers/download.ex
+++ lib/kkalb_web/controllers/download.ex
@@ -3,7 +3,10 @@ defmodule KkalbWeb.DownloadController do
   use KkalbWeb, :controller
 
   def download_portfolio(conn, _) do
-    file = "priv/static/images/Kevin_Kalb_CV_2024_10.pdf"
+    # filename = "priv/static/Kevin_Kalb_CV_2024_10.pdf"
+    # file = Application.app_dir(:kkalb, filename)
+    file = :kkalb |> :code.priv_dir() |> Path.join("static/images/Kevin_Kalb_CV_2024_10.pdf")
+    IO.inspect(file)
     send_download(conn, {:file, file})
   end
 end
