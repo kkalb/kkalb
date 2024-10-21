4adbaa262305a6b74c0513c927a0413e136d9cb0
kkalb <kevinkalb17@googlemail.com>
2024-10-21 17:45:15 +0000

    rework of portfolio

--- lib/kkalb_web/live/home.ex
+++ lib/kkalb_web/live/home.ex
@@ -8,7 +8,67 @@ defmodule KkalbWeb.Live.Home do
 
   @impl Phoenix.LiveView
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    journey = [
+      %{id: 0, title: "Caregiving", date: "2024 - now ", text: "Caregiving for a direct relative. Since this ended now,
+        I am happy to be back in tech."},
+      %{
+        id: 1,
+        title: "Sr. Software Engineer - Team Internet",
+        date: "2023 - 2024",
+        text: "Elixir-driven AI for proper advertising.
+        \"Placeholder for future text enforcing a larger text block for developing\""
+      },
+      %{
+        id: 2,
+        title: "Sr. Software Engineer - Aponia",
+        date: "2021 - 2023",
+        text: "Front- and backend developer, implementing stateof-the-art Git/SCM and SCRUM processes.
+      Implementing, improvement and supervising of a
+      CI/CD pipeline as a DevOps engineer."
+      },
+      %{
+        id: 3,
+        title: "Software Engineer - GE Additive",
+        date: "2019 - 2021",
+        text: "Building software to assess the quality of the melting
+      process (QMMeltpool3D). Creation of intermediate
+      API wrapping software as a man-in-the-middle for
+      different systems inside the additive ecosystem."
+      },
+      %{
+        id: 4,
+        title: "Software Engineer - Hans Weber",
+        date: "2018 - 2019",
+        text: "Optimization of a PID controller for a temperature
+      control loop of an extruder. Creating frontend for
+      reading and writing OPC UA API variables. Analysis
+      and evaluation of the complete control loop."
+      },
+      %{
+        id: 5,
+        title: "Software Engineer - Loewe Technology",
+        date: "2017",
+        text: "Creation of C# software for communication between
+      oscilloscopes via OOP, GUI and VISA driver creation.
+      Translated a Visual Basic driver to C# for
+      communication with an AC source."
+      },
+      %{
+        id: 6,
+        title: "Bachelor of Engineering",
+        date: "2015 - 2019",
+        text: "Electrical Engineering, Automation and Robotics"
+      },
+      %{
+        id: 7,
+        title: "In development",
+        date: "20av - 20xy",
+        text: "Some dummy for more content for developing"
+      }
+    ]
+
+    focus = 1
+    {:ok, assign(socket, focus: focus, journey: journey)}
   end
 
   @impl Phoenix.LiveView
@@ -17,52 +77,23 @@ defmodule KkalbWeb.Live.Home do
   end
 
   @impl Phoenix.LiveView
+  def handle_event("focus", %{"id" => id}, socket) do
+    {:noreply, assign(socket, focus: String.to_integer(id))}
+  end
+
+  @impl Phoenix.LiveView
   def render(assigns) do
     ~H"""
-    <div class="h-full w-full overflow-auto">
+    <div class="h-full w-full">
       <.header text="Portfolio - WIP"></.header>
       <.button type="button" phx-click="download_portfolio" class="absolute top-2 right-1 sm:right-8">
         <span>Download Portfolio</span>
       </.button>
-      <.left_row title="Caregiving" date="2024 - now ">
-        Caregiving for a direct relative. Since this ended now,
-        I am happy to be back in tech.
-      </.left_row>
 
-      <.right_row title="Sr. Software Engineer - Team Internet" date="2023 - 2024">
-        Elixir-driven AI for proper advertising
+      <.right_row :for={ele <- @journey} title={ele.title} date={ele.date} id={ele.id} focus={@focus}>
+        <%= ele.text %>
       </.right_row>
 
-      <.left_row title="Sr. Software Engineer - Aponia" date="2021 - 2023">
-        Front- and backend developer, implementing stateof-the-art Git/SCM and SCRUM processes.
-        Implementing, improvement and supervising of a
-        CI/CD pipeline as a DevOps engineer.
-      </.left_row>
-
-      <.right_row title="Software Engineer - GE Additive" date="2019 - 2021">
-        Building software to assess the quality of the melting
-        process (QMMeltpool3D). Creation of intermediate
-        API wrapping software as a man-in-the-middle for
-        different systems inside the additive ecosystem.
-      </.right_row>
-
-      <.left_row title="Sr. Software Engineer - Hans Weber" date="2018 - 2019">
-        Optimization of a PID controller for a temperature
-        control loop of an extruder. Creating frontend for
-        reading and writing OPC UA API variables. Analysis
-        and evaluation of the complete control loop
-      </.left_row>
-
-      <.right_row title="Bachelor of Engineering" date="2015 - 2019">
-        Electrical Engineering, Automation and Robotics
-      </.right_row>
-
-      <.left_row title="Software Engineer - Loewe Technology" date="2017">
-        Creation of C# software for communication between
-        oscilloscopes via OOP, GUI and VISA driver creation.
-        Translated a Visual Basic driver to C# for
-        communication with an AC source
-      </.left_row>
       <.footer />
     </div>
     """
@@ -70,30 +101,16 @@ defmodule KkalbWeb.Live.Home do
 
   attr :title, :string, required: true
   attr :date, :string, required: true
-  slot :inner_block, required: true
-
-  def left_row(assigns) do
-    ~H"""
-    <div class="grid grid-cols-9 justify-center items-center place-items-center w-full h-full">
-      <.textblock title={@title} class="md:ml-24 ml-4">
-        <%= render_slot(@inner_block) %>
-      </.textblock>
-      <.divider date={@date} dir="left" />
-      <div />
-    </div>
-    """
-  end
-
-  attr :title, :string, required: true
-  attr :date, :string, required: true
+  attr :id, :integer, required: true
+  attr :focus, :integer, required: true
   slot :inner_block, required: true
 
   def right_row(assigns) do
     ~H"""
-    <div class="grid grid-cols-9 justify-center items-center place-items-center w-full h-full">
-      <div class="col-span-4 col-start-1" />
-      <.divider date={@date} dir="right" />
-      <.textblock title={@title} class="sm:mr-8 mr-0">
+    <div class="grid grid-cols-4 justify-center items-center place-items-center w-full h-full">
+      <div class="col-span-1 col-start-1" />
+      <.divider date={@date} focus={@focus == @id} dir="right" />
+      <.textblock title={@title} id={@id} focus={@focus}>
         <%= render_slot(@inner_block) %>
       </.textblock>
     </div>
@@ -101,41 +118,67 @@ defmodule KkalbWeb.Live.Home do
   end
 
   attr :title, :string, required: true
-  attr :class, :string, required: false
+  attr :id, :integer, required: true
+  attr :focus, :boolean, required: true
   slot :inner_block, required: true
 
   def textblock(assigns) do
+    focused? = assigns.id == assigns.focus
+    cond_class = if focused?, do: "text-cwhite w-3/4", else: "text-cwhite/50 w-2/4 hover:animate-pulse"
+    assigns = assign(assigns, cond_class: cond_class, focused?: focused?)
+
     ~H"""
-    <div class={[
-      "sm:block col-span-4 p-4 pt-2 mb-4 bg-cgray/80 border border-corange/50 text-cwhite rounded-md max-w-screen-sm",
-      @class
-    ]}>
-      <p class="font-bold text-xl whitespace-normal mb-2">
+    <button
+      phx-click="focus"
+      phx-value-id={@id}
+      class={[
+        "sm:block col-span-2 p-4 pt-2 mb-4 bg-cgray/80 border border-corange/50 text-cwhite rounded-md sm:mr-8 mr-2",
+        @cond_class
+      ]}
+    >
+      <p :if={not @focused?} class="font-normal text-normal whitespace-normal mb-2">
         <%= @title %>
       </p>
-      <p class="whitespace-normal">
+      <p :if={@focused?} class="font-bold text-xl whitespace-normal mb-2">
+        <%= @title %>
+      </p>
+      <p :if={not @focused?} class="whitespace-normal text-xs">
         <%= render_slot(@inner_block) %>
       </p>
-    </div>
+      <p :if={@focused?} class="whitespace-normal">
+        <%= render_slot(@inner_block) %>
+      </p>
+    </button>
     """
   end
 
   attr :date, :string, required: true
+  attr :focus, :boolean, required: true
   attr :dir, :string, required: true
 
   def divider(assigns) do
+    text_color = if assigns.focus, do: "text-cwhite", else: "text-cwhite/20"
+    bg_color = if assigns.focus, do: "bg-cwhite", else: "bg-cwhite/20"
+    width = if assigns.focus, do: "w-1", else: "w-0.5"
+    circle = if assigns.focus, do: "w-5 h-5", else: "w-4 h-4"
+
+    assigns =
+      assign(assigns,
+        text_color: text_color,
+        bg_color: bg_color,
+        width: width,
+        circle: circle
+      )
+
     ~H"""
-    <div class="flex items-center flex-col text-cwhite h-full whitespace-nowrap">
+    <div class={["flex items-center flex-col h-full whitespace-nowrap", @text_color]}>
       <div class="flex flex-row items-center">
         <div :if={@dir == "right"} class="absolute">
-          <div class="relative right-24"><%= @date %></div>
+          <p class={["relative right-24"]}><%= @date %></p>
         </div>
-        <div class="relative bg-cwhite/80 w-4 h-4 rounded-[50%]" />
-        <div :if={@dir == "left"} class="absolute">
-          <div class="relative left-8"><%= @date %></div>
-        </div>
+        <div class={["relative rounded-[50%]", @bg_color, @circle]} />
       </div>
-      <div class="bg-cwhite w-0.5 h-full my-2"></div>
+      <div class={["h-full my-2", @bg_color, @width]}></div>
     </div>
     """
   end
