f4a03caf61a756ae7e8340b86449cb3e86d76b9b
kkalb <kevinkalb17@googlemail.com>
2024-12-16 09:47:31 +0000

    improve logging for worker

--- lib/kkalb/workers/github_fetcher_worker.ex
+++ lib/kkalb/workers/github_fetcher_worker.ex
@@ -1,7 +1,7 @@
 defmodule Kkalb.Workers.GithubFetcherWorker do
   @moduledoc """
   Trigger with
-  iex -S mix ps
+  ```iex -S mix ps```
   and one of the follwing in the console to start the job scheduler
   ```
   Kkalb.Workers.GithubFetcherWorker.new(%{"backfill_date" => Date.utc_today()}) |> Oban.insert()
@@ -19,10 +19,7 @@ defmodule Kkalb.Workers.GithubFetcherWorker do
 
   @spec perform(map()) :: :ok
   def perform(%{"backfill_date" => backfill_date}) do
-    IO.inspect(backfill_date, label: "perform matched")
-    date = backfill_date
-
-    {rate_limit_remaining, rate_limit_reset, issues_created} = InsertIssues.run_task(100, date)
+    {rate_limit_remaining, rate_limit_reset, issues_created} = InsertIssues.run_task(100, backfill_date)
 
     rate_limit_remaining = String.to_integer(rate_limit_remaining)
 
@@ -30,21 +27,18 @@ defmodule Kkalb.Workers.GithubFetcherWorker do
 
     _ =
       cond do
-        Date.before?(date, @first_issue_date) ->
+        Date.before?(backfill_date, @first_issue_date) ->
           Logger.info("Stop rescheduling since date reached #{backfill_date}")
 
         # when we reached the rate limit, we reschedule the same date but wait longer
         rate_limit_remaining <= 1 ->
-          IO.inspect(rate_limit_remaining, label: "rate_limit_remaining")
-          IO.inspect(rate_limit_reset, label: "rate_limit_reset")
-          IO.inspect(wait_time, label: "wait_time")
           Process.sleep(wait_time)
-          perform(%{"backfill_date" => Date.add(date, -14)})
+          perform(%{"backfill_date" => Date.add(backfill_date, -14)})
 
         # as long as the backfill date is later than the stop date, we continue rescheduling
         true ->
-          backfill_date = if issues_created >= 100, do: Date.add(date, 30), else: Date.add(date, -60)
-          IO.inspect(issues_created)
+          backfill_date = if issues_created >= 100, do: Date.add(backfill_date, 30), else: Date.add(backfill_date, -60)
+          Logger.info("#{backfill_date}: Issues created: #{issues_created}. Continuing after #{wait_time} ms ...")
           Process.sleep(wait_time)
           perform(%{"backfill_date" => backfill_date})
       end
@@ -53,13 +47,9 @@ defmodule Kkalb.Workers.GithubFetcherWorker do
   end
 
   @doc """
-  When backfill_date not provided, we take the first issue date.
+  When backfill_date not provided, do it once for today.
   """
-  def perform(args) do
-    IO.inspect(args, label: "perform unmatched")
-
-    perform(%{"backfill_date" => Date.utc_today(), "schedule_in" => 0})
-  end
+  def perform(_args), do: InsertIssues.run_task(100, Date.utc_today())
 
   # decelerate when limit gets slower
   # and rate_limit_remaining is between 0 and 30.
@@ -71,7 +61,6 @@ defmodule Kkalb.Workers.GithubFetcherWorker do
       |> DateTime.from_unix!()
       |> DateTime.diff(DateTime.utc_now(), :second)
 
-    IO.inspect(seconds_remaining, label: "seconds_remaining")
     Logger.info("Remaining rate #{rate_limit_remaining} for #{seconds_remaining} s")
     # we try to never reach the limit, since there can be a secondary
     # limit which we ensure not to reach with this
