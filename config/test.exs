b6b662852e693470adb797739de5d8c581a3eabd
kkalb <kevinkalb17@googlemail.com>
2025-02-18 20:37:01 +0000

    add and fix test

--- config/test.exs
+++ config/test.exs
@@ -16,6 +16,7 @@ config :kkalb, KkalbWeb.Endpoint,
   secret_key_base: "8A0Aq6HADTR1KytrgJp0SvrRvt9LzinxI+Lz+eQwAaBVjXDb6CpwUb4NjtugeVKu",
   server: true
 
+config :kkalb, :issue_storage, Kkalb.Issues
 config :kkalb, :sandbox, Sandbox
 
 # Print only warnings and errors during test
@@ -24,5 +25,25 @@ config :logger, level: :warning
 # Initialize plugs at runtime for faster test compilation
 config :phoenix, :plug_init_mode, :runtime
 
-config :wallaby, :chromedriver, binary: "/usr/local/bin/chromedriver"
-config :wallaby, otp_app: :kkalb, driver: Wallaby.Chrome
+# TODO: enable headless on CI
+config :wallaby,
+  driver: Wallaby.Chrome,
+  otp_app: :kkalb,
+  hackney_options: [timeout: :infinity, recv_timeout: :infinity, pool: :wallaby_pool],
+  chromedriver: [
+    headless: true,
+    binary: "/opt/chrome/chrome",
+    path: "/usr/local/bin/chromedriver",
+    capabilities: %{
+      chromeOptions: %{
+        args: [
+          "--headless",
+          "--no-sandbox",
+          "window-size=1600,900",
+          "--fullscreen",
+          "--disable-gpu",
+          "--disable-dev-shm-usage"
+        ]
+      }
+    }
+  ]
--- test/kkalb_web/live/github_issue_vis/index_test.exs
+++ test/kkalb_web/live/github_issue_vis/index_test.exs
@@ -2,10 +2,8 @@ defmodule KkalbWeb.GithubIssueVis.IndexTest do
   use KkalbWeb.FeatureCase, async: false
 
   feature "User visits the homepage", %{session: session} do
-    IO.inspect(session)
-
     session
     |> visit("/")
-    |> assert_text("Welcome to Phoenix!")
+    |> assert_text("Portfolio")
   end
 end
